6d31471759460f96e3d271ed107e1af6f6370d76
Mehmet Erhan Derya <erherh42@gmail.com>
2026-05-02 04:57:02 +0300

    feat: add endpoint to retrieve posts by user and refactor response type for MyPosts

--- OmniFlow/OmniFlow.WebApi/Controllers/v1/UsersController.cs
+++ OmniFlow/OmniFlow.WebApi/Controllers/v1/UsersController.cs
@@ -2,7 +2,10 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OmniFlow.Application.DTOs.Posts;
 using OmniFlow.Application.Features.Posts.Queries.GetMyPosts;
+using OmniFlow.Application.DTOs.Posts;
 using OmniFlow.Application.DTOs.Users;
+using OmniFlow.Application.Features.Posts.Queries.GetMyPosts;
+using OmniFlow.Application.Features.Posts.Queries.GetPostsByUser;
 using OmniFlow.Application.Features.Users.Commands.UpdateProfile;
 using OmniFlow.Application.Features.Users.Queries.GetTopContributors;
 using OmniFlow.Application.Features.Users.Queries.GetUserProfile;
@@ -41,12 +44,34 @@ public class UsersController : BaseApiController
 	}
 
 	[HttpGet("me/posts")]
-	[ProducesResponseType(typeof(OmniFlow.Application.Wrappers.PagedResponse<PostResponse>), StatusCodes.Status200OK)]
+	[ProducesResponseType(typeof(PagedResponse<PostResponse>), StatusCodes.Status200OK)]
 	[ProducesResponseType(StatusCodes.Status401Unauthorized)]
 	public async Task<IActionResult> MyPosts([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 20)
 	{
 		var result = await Mediator.Send(new GetMyPostsQuery
 		{
+			PageNumber = pageNumber,
+			PageSize = pageSize
+		});
+
+		return Ok(result);
+	}
+
+	/// <summary>
+	/// Returns the visible community posts for a specific user.
+	/// </summary>
+	[HttpGet("{userId:guid}/posts")]
+	[ProducesResponseType(typeof(PagedResponse<PostResponse>), StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	public async Task<IActionResult> GetPostsByUser(
+		[FromRoute] Guid userId,
+		[FromQuery] int pageNumber = 1,
+		[FromQuery] int pageSize = 20)
+	{
+		var result = await Mediator.Send(new GetPostsByUserQuery
+		{
+			UserId = userId,
 			PageNumber = pageNumber,
 			PageSize = pageSize
 		});
